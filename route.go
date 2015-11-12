69a6cf850cdaabc1b8030f2bccc890fe220a03a4
Secret <haichuang221@163.com>
2015-11-12 23:59:28 +0800

    refinement: move the logic of route from tcp_example to itself

--- route.go
+++ route.go
@@ -4,19 +4,24 @@ import (
 	"fmt"
 )
 
-type RouteFun func(params map[string]string, c *Client) IMessage
+type RouteFun func(client *Client, params map[string]string) IMessage
 
-type RouterList map[string]RouteFun
+type RouterList map[string]map[string]RouteFun
 
 func (self RouterList) RouteMsg(client *Client, msg string) bool {
 	fmt.Printf("route %v msg:%s", client, msg)
 
 	requestMsg := NewMessage(client, msg)
 	params := requestMsg.Get()
-	if route, ok := self[params["feature"]]; ok {
+	if router, ok := self[params["feature"]]; ok {
 		// is it necessary of setting this in a goruntine?
 		go func() {
-			responseMsg := route(params, client)
+			var responseMsg IMessage
+			if f, ok := router[params["command"]]; ok {
+				responseMsg = f(client, params)
+			} else {
+				responseMsg = NewMessage(client, fmt.Sprintf("no '%s' command", params["command"]))
+			}
 			responseMsg.exec()
 		}()
 		return true
